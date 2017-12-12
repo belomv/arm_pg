7fe4904ad7e99961924ff03e88497b5d1458ecd6
potamoshnev-dv <proho@dimka.pro>
2017-12-12 22:34:04 +0300

    First2.0Commit

---
+++ pushgate-arm-webapp/src/main/java/arm/pushgate/pushgate_arm_webapp/App.java
@@ -0,0 +1,13 @@
+package arm.pushgate.pushgate_arm_webapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
